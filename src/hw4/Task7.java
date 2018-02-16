79284cb56b8b2abeca2a6160c2256fa7616ceade
akuzikovv@gmail.com <akuzikovv@gmail.com>
2018-02-16 14:28:10 +0200

    task7

--- src/hw4/Task7.java
+++ src/hw4/Task7.java
@@ -1,12 +1,12 @@
 package hw4;
 
 public class Task7 extends Main {
-/*74. Сортировка обменами. Дана последовательность чисел а 1 , а 2 , ..., а n .
-Требуется переставить числа в порядке возрастания. Для этого сравниваются два соседних числа аi  и a i+1 .
-Если а i  больше а i+1 , то делается перестановка.
-Так продолжается до тех пор, пока все элементы не станут расположены в порядке возрастания.
-Составить алгоритм сортировки, подсчитывая при этом количество перестановок.*/
-public static int i, j, a, max, index;
+    /*74. Сортировка обменами. Дана последовательность чисел а 1 , а 2 , ..., а n .
+    Требуется переставить числа в порядке возрастания. Для этого сравниваются два соседних числа аi  и a i+1 .
+    Если а i  больше а i+1 , то делается перестановка.
+    Так продолжается до тех пор, пока все элементы не станут расположены в порядке возрастания.
+    Составить алгоритм сортировки, подсчитывая при этом количество перестановок.*/
+    public static int n3, i, j, a, max, index;
     public static int proizv = 1;
     public static int mas[] = new int[100];
     public static int result[] = new int[mas.length];
@@ -15,7 +15,7 @@ public static int i, j, a, max, index;
         System.out.print("Массив случайных чисел от 1 до 99 : " + '\n');
         fillingMassiv();
         displayMassiv();
-//        countingIndex();
+        zamena();
         Main.menu();
     }
 
@@ -25,20 +25,38 @@ public static int i, j, a, max, index;
     }
 
     public static void fillingMassiv() {
-        for (i = 0; i <= 49; i++) {
-
-                a = generateRandom();
-                mas[i] = a;
-
+        for (i = 0; i <= 9; i++) {
+            a = generateRandom();
+            mas[i] = a;
         }
     }
 
     public static void displayMassiv() {
-        for (i = 0; i <=49; i++) {
-
-                System.out.print(mas[i] + ",");
-//            System.out.print("\n");
+        for (i = 0; i <= 9; i++) {
+            System.out.print(mas[i] + ",");
         }
+        System.out.println();
+
     }
 
+    public static void zamena() {
+        for (i = 0; i <= 9; i++) {
+            for (j = 1; j <= 9; j++) {
+                int n1 = mas[i];
+                int n2 = mas[j];
+                if (mas[i] > mas[j]) {
+                    mas[i] = n2;
+                    mas[j] = n1;
+                    mas[a] = mas[i];
+                    mas[i]=n1;
+                    System.out.print(mas[a] + ",");
+                } else {
+                    mas[a] = mas[i];
+                    System.out.print(mas[a] + ",");
+                    mas[i]=n1;
+                    i++;
+                }
+            }
+        }
+    }
 }
