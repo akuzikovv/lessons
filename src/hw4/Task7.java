39a052b5a2a75f60832e5dd25f02cfb1de96c8cf
akuzikovv@gmail.com <akuzikovv@gmail.com>
2018-02-22 12:34:34 +0200

    hw4_Task7_fix

--- src/hw4/Task7.java
+++ src/hw4/Task7.java
@@ -1,19 +1,17 @@
 package hw4;
-
-import hw1.Exit;
-
 public class Task7 extends Main {
     /*74. Сортировка обменами. Дана последовательность чисел а 1 , а 2 , ..., а n .
     Требуется переставить числа в порядке возрастания. Для этого сравниваются два соседних числа аi  и a i+1 .
     Если а i  больше а i+1 , то делается перестановка.
     Так продолжается до тех пор, пока все элементы не станут расположены в порядке возрастания.
     Составить алгоритм сортировки, подсчитывая при этом количество перестановок.*/
-    public static int n3, i, j, a;
+    public static int n3, i, j,index;
     public static int qty = 0;
     public static int mas[] = new int[100];
 
     public static void main() {
         System.out.print("Массив случайных чисел от 1 до 99 : " + '\n');
+        qty=0;
         fillingRandomMassiv();
         zamena();
         Main.menu();
@@ -42,42 +40,42 @@ public class Task7 extends Main {
     }
 
     public static void zamena() {
-            n3 = 0;
-            for (i = 0; i <= 9; i++) {
-                for (j = 1; j <= 10; j++) {
-                    int num1 = mas[i];
-                    int num2 = mas[j];
-                    if (num2 == 0) {
-                        if (qty==9){
-                            System.out.print(mas[i] + "." + '\n');
-                            System.out.println("Stop" + '\n');
-                            Main.menu();
-                        }else {
+        n3 = 0;
+        for (i = 0; i <= 9; i++) {
+            for (j = 1; j <= 10; j++) {
+                int num1 = mas[i];
+                int num2 = mas[j];
+                if (num2 == 0) {
+                    if (index ==9) {
+                        System.out.print(mas[i] + "." + '\n');
+                        System.out.println("Количество перестановок: " +qty+ '\n');
+                        Main.menu();
+                    } else {
                         System.out.print(mas[i] + "." + '\n');
                         fillingMassivAfterSorting();
-                        qty=0;//для остановки
+                        index = 0;
                         zamena();
-                    }}
-                    if (mas[i] > mas[j]) {
-                        mas[i] = num2;
-                        mas[j] = num1;
-                        System.out.print(mas[i] + ",");
-                        mas[i] = num1;
-                        mas[n3] = num2;
-                        i++;
-                        n3++;
-                        qty = 0;
-
-                    } else {
-                        System.out.print(mas[i] + ",");
-                        mas[i] = num2;
-                        mas[n3] = num1;
-                        i++;
-                        n3++;
-qty++;
                     }
                 }
+                if (mas[i] > mas[j]) {
+                    mas[i] = num2;
+                    mas[j] = num1;
+                    System.out.print(mas[i] + ",");
+                    mas[i] = num1;
+                    mas[n3] = num2;
+                    i++;
+                    n3++;
+                    index =0;
+                    qty++;
+                } else {
+                    System.out.print(mas[i] + ",");
+                    mas[i] = num2;
+                    mas[n3] = num1;
+                    i++;
+                    n3++;
+                    index++;
+                }
             }
-
+        }
     }
 }
