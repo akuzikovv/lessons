b403f6513f0d2d57de992c49f83d31ef0dd15758
akuzikovv@gmail.com <akuzikovv@gmail.com>
2018-02-23 13:58:16 +0200

    hw7

--- src/hw7/Main.java
+++ src/hw7/Main.java
@@ -1,29 +1,23 @@
 package hw7;
-
-
 import hw7.Person.abstr.Person;
 import hw7.Person.abstr.Student;
 import hw7.Person.Student.CollegeStudent;
 import hw7.Person.Teacher.Teacher;
-
 import java.util.Scanner;
-
 
 public class Main {
     public static Scanner scanner = new Scanner(System.in);
-     public static int mas[];
-   public static Person person;
+    public static int mas[];
+    public static Person person;
+    public static Student student;
+    public static CollegeStudent collegeStudent;
+    public static Teacher teacher;
 
     public static void main(String[] args) {
-
-        Student student;
-        CollegeStudent collegeStudent;
-        Teacher teacher;
-        switch (scanner.nextInt()){
-            case 1 :
+        switch (scanner.nextInt()) {
+            case 1:
                 System.out.println("Добавьте персонажей");
                 addpersons();
-
                 break;
             case 2:
                 System.out.println("Отобразите персонажей");
@@ -34,56 +28,101 @@ public class Main {
         }
     }
 
-    public static void addpersons(){
-        switch (scanner.nextInt()){
-            case 1 :
+    public static void addpersons() {
+        System.out.println("1: Добавить персону");
+        System.out.println("2: Добавить Студента");
+        System.out.println("3: Добавить Учителя");
+        System.out.println("4: Добавить Студента Коледжа");
+        switch (scanner.nextInt()) {
+            case 1:
                 System.out.println("Введите персону");
                 Main.addperson();
-
                 break;
-//            case 2:
-//
-//                break;
-//            case 3:  collegeStudent = new CollegeStudent(scanner.nextLine(),
-//                    scanner.nextInt(),scanner.nextLine(),
-//                    scanner.nextLine(),scanner.nextInt(),
-//                    scanner.nextInt(),scanner.nextLine());
-//            case 4: teacher = new Teacher(scanner.nextLine(),scanner.nextInt(),
-//                    scanner.nextLine(),scanner.nextLine(),scanner.nextDouble());
+            case 2:
+                System.out.println("Введите Студента");
+                Main.addStudent();
+                break;
+            case 3:
+                System.out.println("Введите Учителя");
+                Main.addTeacher();
+                break;
+            case 4:
+                System.out.println("Введите Студента Коледжа");
+                Main.addCollegeStudent();
+                break;
             default:
                 System.out.println("QWERTY");
         }
     }
 
-    public static void displayPersons(){
-        switch (scanner.nextInt()){
-            case 1 :
-                System.out.println("ОТобразите персону");
-                Main.displayperson();
-
+    public static void displayPersons() {
+        System.out.println("1: Отобразить персону");
+        System.out.println("2: Отобразить Студента");
+        System.out.println("3: Отобразить Учителя");
+        System.out.println("4: Отобразить Студента Коледжа");
+        switch (scanner.nextInt()) {
+            case 1:
+                System.out.println("Отображение персоны");
+                Main.displayPerson();
                 break;
-//            case 2:  student = new Student(scanner.nextLine(),scanner.nextInt(),scanner.nextLine(),scanner.nextLine(),scanner.nextInt());
-//                break;
-//            case 3:  collegeStudent = new CollegeStudent(scanner.nextLine(),
-//                    scanner.nextInt(),scanner.nextLine(),
-//                    scanner.nextLine(),scanner.nextInt(),
-//                    scanner.nextInt(),scanner.nextLine());
-//            case 4: teacher = new Teacher(scanner.nextLine(),scanner.nextInt(),
-//                    scanner.nextLine(),scanner.nextLine(),scanner.nextDouble());
+            case 2:
+                System.out.println("Отображение Студента");
+                Main.displayStudent();
+                break;
+            case 3:
+                System.out.println("Отображение Учителя");
+                Main.displayTeacher();
+                break;
+            case 4:
+                System.out.println("Отображение Студента Коледжа");
+                Main.displayCollegeStudent();
+                break;
             default:
                 System.out.println("QWERTY");
         }
     }
 
-    public static void addperson(){
+    public static void addperson() {
         String name = scanner.nextLine();
-//        int age = scanner.nextInt();
-        person = new Person(name,25,"m");
+        person = new Person(name, 25, "m");
         System.out.println("ОТобразите персону");
         System.out.println(person.toString());
     }
 
-    public static void displayperson(){
+    public static void addStudent() {
+        String name = scanner.nextLine();
+        person = new Person(name, 25, "m");
+        System.out.println("ОТобразите персону");
         System.out.println(person.toString());
+    }
+
+    public static void addTeacher() {
+        String name = scanner.nextLine();
+        person = new Person(name, 25, "m");
+        System.out.println("ОТобразите персону");
+        System.out.println(person.toString());
+    }
+
+    public static void addCollegeStudent() {
+        String name = scanner.nextLine();
+        person = new Person(name, 25, "m");
+        System.out.println("ОТобразите персону");
+        System.out.println(person.toString());
+    }
+
+    public static void displayPerson() {
+        System.out.println(person.toString());
+    }
+
+    public static void displayStudent() {
+        System.out.println(student.toString());
+    }
+
+    public static void displayTeacher() {
+        System.out.println(teacher.toString());
+    }
+
+    public static void displayCollegeStudent() {
+        System.out.println(collegeStudent.toString());
     }
 }
